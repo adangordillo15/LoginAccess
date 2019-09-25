72dd71f46676a7486cb3d8aeb1e2b2c8b00df677
adangordillo15 <52899271+adangordillo15@users.noreply.github.com>
2019-09-25 14:53:31 -0500

    Add files via upload

---
+++ Domain/UserModel.vb
@@ -0,0 +1,7 @@
+﻿Imports DataAccess
+Public Class UserModel
+    Dim userDao As New UserDao()
+    Public Function Login(email As String, password As String) As Boolean
+        Return userDao.Login(email, password)
+    End Function
+End Class
